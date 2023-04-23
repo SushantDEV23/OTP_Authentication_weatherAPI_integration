5aa4d4641d43d3ac8beb47ba90711f930d2c7eed
Sushant Nerkar <sushantnerkarjbp@gmail.com>
2023-04-24 00:12:30 +0530

    Some Fixes

--- verification/access/urls.py
+++ verification/access/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import *
+from access.views import register, home, login_attempt, login_otp, otp, weather
 urlpatterns=[
     path('', home, name='home'),
-    path('login_attempt', login_attempt, name='login_attempt'),
-    path('register', register, name='register'),
-    path('otp', otp, name='otp'),
-    path('weather', weather, name='weather'),
-    path('login_otp', login_otp, name='login_otp')
+    path('login_attempt/', login_attempt, name='login'),
+    path('register/', register, name='register'),
+    path('otp/', otp, name='otp'),
+    path('weather/', weather, name='weather'),
+    path('login_otp/', login_otp, name='login_otp')
 ]
